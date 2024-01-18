408c98b88e8f78d23ce7e385f2fc0bf9b0adbbae
Marcos Matsuda <marcosmatsuda.ti@gmail.com>
2024-01-17 23:20:40 -0300

    feat: Updated Homescreen with Modal (#13)
    * feat: Update mockData
    
    * feat: Updated package.json with material-bottom-tabs
    
    * feat: Added BottomNavigation
    
    * feat: Added new DictionaryScreen
    
    * feat: Updated Homescreen with Modal

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,65 +1,47 @@
 import React, {useEffect, useState} from 'react';
-import {View, FlatList, StyleSheet, Text} from 'react-native';
-import {Searchbar, List} from 'react-native-paper';
-// import {useNavigation} from '@react-navigation/native';
-// import {MainScreenNavigationProp} from '@navigation/types';
-
+import {View, StyleSheet, Text} from 'react-native';
+import {
+  Modal,
+  Portal,
+  Button,
+  PaperProvider,
+  Searchbar,
+} from 'react-native-paper';
 import {useNetInfo} from '@react-native-community/netinfo';
-
-// import mockedData from './../../mocks/mockData';
-
-const Colors = {
-  primary: '#3498db',
-  secondary: '#2ecc71',
-  accent: '#e74c3c',
-  background: '#ecf0f1',
-  text: '#2c3e50',
-};
-
-interface ListIconProps {
-  icon: string;
-}
-
-const ListIcon: React.FC<ListIconProps> = ({icon}) => (
-  <List.Icon color={Colors.primary} icon={icon} />
-);
-
-interface ItemProps {
-  title: string;
-  renderLeft: (props: any) => React.ReactNode;
-}
-
-const Item: React.FC<ItemProps> = ({title, renderLeft}) => (
-  <List.Item title={title} left={renderLeft} />
-);
-
-interface DataItem {
-  [key: string]: string;
-}
+import mockedData from './../../mocks/mockData';
 
 const HomeScreen: React.FC = () => {
   const {isConnected} = useNetInfo();
 
-  // const navigation = useNavigation<MainScreenNavigationProp>();
+  const [searchVal, setSearchVal] = useState<string>('');
+  const [data, setData] = useState<any[] | null>(null);
+  const [visible, setVisible] = React.useState(false);
 
-  const [data, setData] = useState<DataItem[] | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [searchQuery, setSearchQuery] = useState<string>('');
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {backgroundColor: 'white', padding: 20};
 
-  const handleSearch = () => {
-    // navigation.navigate('DetailsScreen');
+  const dataResults = () => {
+    if (!data) {
+      return [];
+    }
+
+    const filteredResults = data.filter(item =>
+      item.name.toLowerCase().includes(searchVal.toLowerCase()),
+    );
+
+    return filteredResults;
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         await new Promise(resolve => setTimeout(resolve, 2000));
-
-        // setData(mockedData);
+        setData(mockedData);
       } catch (e) {
         throw e;
       } finally {
-        setLoading(false);
+        // setLoading(false);
       }
     };
 
@@ -74,40 +56,32 @@ const HomeScreen: React.FC = () => {
     );
   }
 
-  const renderLeft = (props: any) => <ListIcon {...props} icon="star" />;
-
   return (
     <View style={styles.container}>
-      <Searchbar
-        placeholder="Search"
-        onChangeText={handleSearch}
-        value={'Something'}
-        style={styles.searchbar}
-      />
-      <FlatList
-        data={data}
-        keyExtractor={item => String(item.id)}
-        renderItem={({item}) => (
-          <Item
-            title={item.title}
-            description={item.description}
-            renderLeft={renderLeft}
-          />
-        )}
-
-      />
-
-      {loading ? (
-        <Text>Carregando...</Text>
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => (
-            <Item title={item.name} renderLeft={renderLeft} />
-          )}
+      <PaperProvider>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={setSearchVal}
+          value={'Something'}
+          style={styles.searchbar}
         />
-      )}
+        <Portal>
+          <Modal
+            visible={visible}
+            onDismiss={hideModal}
+            contentContainerStyle={containerStyle}>
+            <Text>Will show result.</Text>
+            <View>
+              {dataResults().map((item: any, index: number) => (
+                <Text key={index}>{item.name}</Text>
+              ))}
+            </View>
+          </Modal>
+        </Portal>
+        <Button style={{marginTop: 30}} onPress={showModal}>
+          Show
+        </Button>
+      </PaperProvider>
     </View>
   );
 };
